3f1fb900facdff598ba09f67fa82079d44dddf67
Sakari Angervuori <sakari.angervuori@hotmail.com>
2021-01-17 10:02:04 +0200

    sakari

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log('Hello console!');
+const submit = document.querySelector('.submit');
+const languageSubmit = document.querySelector('.languageField');
+
+const coursesEn = ["Hamburger, cream sauce and poiled potates",
+                "Goan style fish curry and whole grain rice",
+                "Vegan Chili sin carne and whole grain rice",
+                "Broccoli puree soup, side salad with two napas",
+                "Lunch baguette with BBQ-turkey filling",
+                "Cheese / Chicken / Vege / Halloum burger and french fries"];
+
+const coursesFi = ["Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
+                "Goalaista kalacurrya ja täysjyväriisiä",
+                "vegaani Chili sin carne ja täysjyväriisi",
+                "Parsakeittoa,lisäkesalaatti kahdella napaksella",
+                "Lunch baguette with BBQ-turkey filling",
+                "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];
+
+function teksti() {
+                    var kieli = languageSubmit.value;
+                    if(kieli === 'suomi'){
+                        courses = coursesFi;
+                        document.getElementById("box1").innerHTML = coursesFi.map(coursesFi => coursesFi);
+                    }
+
+                    if(kieli === 'english'){
+                        courses = coursesEn;
+                        document.getElementById("box1").innerHTML = coursesEn.map(coursesEn => coursesEn);
+                    }
+                }
+
+submit.addEventListener('click', teksti);
+
+function sort() {
+                    courses.sort();
+                    /*courses.reverse();*/
+                    document.getElementById("box1").innerHTML = courses;
+                }
+
+                function random() {
+                    var random = courses[Math.floor(Math.random()*courses.length)];
+                    window.alert(random);
+                }
+
