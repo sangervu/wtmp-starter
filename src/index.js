52b4dbfd38daf48d0022ba79e9767eb1a643fb13
Sakari Angervuori <sakari.angervuori@cgi.com>
2021-01-27 19:23:22 +0200

    sakari

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const submit = document.querySelector('.submit');
 const languageSubmit = document.querySelector('.languageField');
 const sortSubmit = document.querySelector('.order');
 const randomSubmit = document.querySelector('.random');
+const fazerMenuFI = document.querySelector('.fazerFI');
+const fazerMenuEN = document.querySelector('.fazerEN');
+
+import FazerMenu from "./fazer-week-example.json";// importataan paikallisessa hakemistossa oleva json tiedosto
+import FazerMenuEN from "./fazer-week-example-en.json";// importataan paikallisessa hakemistossa oleva json tiedosto
+// import SodexoMenu from "./sodexo-day-example.json";// importataan paikallisessa hakemistossa oleva json tiedosto
 
 const coursesEn = ["Hamburger, cream sauce and poiled potates",
   "Goan style fish curry and whole grain rice",
@@ -46,4 +52,61 @@ function random() {
 }
 
 randomSubmit.addEventListener('click', random);
+
+
+function menuFazer() {
+  //window.alert(FazerMenu.LunchMenus[0].DayOfWeek);
+  
+    var fazerMenuListLength = FazerMenu.LunchMenus[0].SetMenus.length; //json importattu jo alussa
+    document.getElementById("box1").innerHTML = "";
+  
+    //    var x;
+    //   for (x in fazerMenuList){
+    //   document.getElementById("box").innerHTML += fazerMenuList[x] + "<br>";
+    //   }
+    // }
+  
+  
+    for (var i = 0; i < fazerMenuListLength; i++) {
+  
+      var fazerMenuList = FazerMenu.LunchMenus[0].SetMenus[i].Meals.map(select);
+      document.getElementById("box1").innerHTML += fazerMenuList + "<br>";
+  
+      function select(item) {
+  
+        var dish = item.Name;
+        return dish;
+      }
+    }
+  
+  }
+  fazerMenuFI.addEventListener('click', menuFazer);
+
+  function menuFazerEN() {
+    //window.alert(FazerMenu.LunchMenus[0].DayOfWeek);
+    
+      var fazerMenuListLength = FazerMenuEN.LunchMenus[0].SetMenus.length; //json importattu jo alussa
+      document.getElementById("box1").innerHTML = "";
+    
+      //    var x;
+      //   for (x in fazerMenuList){
+      //   document.getElementById("box").innerHTML += fazerMenuList[x] + "<br>";
+      //   }
+      // }
+    
+    
+      for (var i = 0; i < fazerMenuListLength; i++) {
+    
+        var fazerMenuList = FazerMenuEN.LunchMenus[0].SetMenus[i].Meals.map(select);
+        document.getElementById("box1").innerHTML += fazerMenuList + "<br>";
+    
+        function select(item) {
+    
+          var dish = item.Name;
+          return dish;
+        }
+      }
+    
+    }
+    fazerMenuEN.addEventListener('click', menuFazerEN);
 
